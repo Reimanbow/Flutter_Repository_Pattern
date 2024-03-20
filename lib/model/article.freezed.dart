18bc90878dbd2b31f7327ce4dcf488a1c029c780
Reimanbow <reimanbow0318@gmail.com>
2024-03-20 23:18:43 +0900

    modelの作成

---
+++ lib/model/article.freezed.dart
@@ -0,0 +1,228 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'article.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Article _$ArticleFromJson(Map<String, dynamic> json) {
+  return _Article.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Article {
+  @JsonKey(name: "created_at")
+  String get createdAt => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get url => throw _privateConstructorUsedError;
+  User get user => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ArticleCopyWith<Article> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ArticleCopyWith<$Res> {
+  factory $ArticleCopyWith(Article value, $Res Function(Article) then) =
+      _$ArticleCopyWithImpl<$Res, Article>;
+  @useResult
+  $Res call(
+      {@JsonKey(name: "created_at") String createdAt,
+      String title,
+      String url,
+      User user});
+
+  $UserCopyWith<$Res> get user;
+}
+
+/// @nodoc
+class _$ArticleCopyWithImpl<$Res, $Val extends Article>
+    implements $ArticleCopyWith<$Res> {
+  _$ArticleCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? createdAt = null,
+    Object? title = null,
+    Object? url = null,
+    Object? user = null,
+  }) {
+    return _then(_value.copyWith(
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as User,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $UserCopyWith<$Res> get user {
+    return $UserCopyWith<$Res>(_value.user, (value) {
+      return _then(_value.copyWith(user: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$ArticleImplCopyWith<$Res> implements $ArticleCopyWith<$Res> {
+  factory _$$ArticleImplCopyWith(
+          _$ArticleImpl value, $Res Function(_$ArticleImpl) then) =
+      __$$ArticleImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {@JsonKey(name: "created_at") String createdAt,
+      String title,
+      String url,
+      User user});
+
+  @override
+  $UserCopyWith<$Res> get user;
+}
+
+/// @nodoc
+class __$$ArticleImplCopyWithImpl<$Res>
+    extends _$ArticleCopyWithImpl<$Res, _$ArticleImpl>
+    implements _$$ArticleImplCopyWith<$Res> {
+  __$$ArticleImplCopyWithImpl(
+      _$ArticleImpl _value, $Res Function(_$ArticleImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? createdAt = null,
+    Object? title = null,
+    Object? url = null,
+    Object? user = null,
+  }) {
+    return _then(_$ArticleImpl(
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as User,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ArticleImpl implements _Article {
+  const _$ArticleImpl(
+      {@JsonKey(name: "created_at") required this.createdAt,
+      required this.title,
+      required this.url,
+      required this.user});
+
+  factory _$ArticleImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ArticleImplFromJson(json);
+
+  @override
+  @JsonKey(name: "created_at")
+  final String createdAt;
+  @override
+  final String title;
+  @override
+  final String url;
+  @override
+  final User user;
+
+  @override
+  String toString() {
+    return 'Article(createdAt: $createdAt, title: $title, url: $url, user: $user)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ArticleImpl &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.url, url) || other.url == url) &&
+            (identical(other.user, user) || other.user == user));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, createdAt, title, url, user);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ArticleImplCopyWith<_$ArticleImpl> get copyWith =>
+      __$$ArticleImplCopyWithImpl<_$ArticleImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ArticleImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Article implements Article {
+  const factory _Article(
+      {@JsonKey(name: "created_at") required final String createdAt,
+      required final String title,
+      required final String url,
+      required final User user}) = _$ArticleImpl;
+
+  factory _Article.fromJson(Map<String, dynamic> json) = _$ArticleImpl.fromJson;
+
+  @override
+  @JsonKey(name: "created_at")
+  String get createdAt;
+  @override
+  String get title;
+  @override
+  String get url;
+  @override
+  User get user;
+  @override
+  @JsonKey(ignore: true)
+  _$$ArticleImplCopyWith<_$ArticleImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
